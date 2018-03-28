03930cfa963914797778e3535c7149040ffa2372
krzysiek28 <krzysztof.uliasz@gmail.com>
2018-03-28 21:19:35 +0200

    add security to server site

--- server/src/main/java/com/studio_projektowe/communicator/repositories/AppUserRepository.java
+++ server/src/main/java/com/studio_projektowe/communicator/repositories/AppUserRepository.java
@@ -4,5 +4,5 @@ import com.studio_projektowe.communicator.entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
-
+    AppUser findByUsername(String username);
 }
